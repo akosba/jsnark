7c919d7517da9fc016f23192a027433c08668772
Ahmed <akosba@cs.umd.edu>
2016-01-15 02:39:45 -0500

    Update in ModGadget + added ModConstantGadget

--- JsnarkCircuitBuilder/src/examples/gadgets/ModConstantGadget.java
+++ JsnarkCircuitBuilder/src/examples/gadgets/ModConstantGadget.java
@@ -11,29 +11,33 @@ import circuit.operations.Gadget;
 import circuit.structure.Wire;
 
 /**
- * This gadget provides the remainder of a % b. 
- * Use within appropriate ranges so that the prover cannot cheat via overflows.
+ * This gadget provides the remainder of a % b, where b is a circuit constant.
  *
  *
  */
 
-public class ModGadget extends Gadget {
+public class ModConstantGadget extends Gadget {
 
 	private Wire a;
-	private Wire b;
+	private BigInteger b;
 	private Wire r;
 	private Wire q;
 
-	private int bitwidth; // bitwidth for both a, b
+	private int bitwidth; // a's bitwidth
 
-	public ModGadget(Wire a,  Wire b, int bitwidth, String...desc) {
+	public ModConstantGadget(Wire a, int bitwidth, BigInteger b, String...desc) {
 		super(desc);
 		this.a = a;
 		this.b = b;
 		this.bitwidth = bitwidth;
-		if(bitwidth > 126){
-			throw new IllegalArgumentException("Bitwidth not supported yet.");
+		if(b.signum() != 1){
+			throw new IllegalArgumentException("b must be a positive constant. Signed operations not supported yet.");
 		}
+		if(bitwidth < b.bitLength()){
+			throw new IllegalArgumentException("a's bitwidth < b's bitwidth -- This gadget is not needed.");
+		}
+		// TODO: add further checks.
+		
 		buildCircuit();
 	}
 
@@ -46,18 +50,18 @@ public class ModGadget extends Gadget {
 			@Override
 			public void evaluate(CircuitEvaluator evaluator) {
 				BigInteger aValue = evaluator.getWireValue(a);
-				BigInteger bValue = evaluator.getWireValue(b);
-				BigInteger rValue = aValue.mod(bValue);
+				BigInteger rValue = aValue.mod(b);
 				evaluator.setWireValue(r, rValue);
-				BigInteger qValue = aValue.divide(bValue);
+				BigInteger qValue = aValue.divide(b);
 				evaluator.setWireValue(q, qValue);
 			}
 
 		});
 		
-		r.restrictBitLength(bitwidth);
-		q.restrictBitLength(bitwidth);
-		generator.addOneAssertion(r.isLessThan(b, bitwidth));
+		int bBitwidth = b.bitLength();
+		r.restrictBitLength(bBitwidth);
+		q.restrictBitLength(bitwidth - bBitwidth + 1);
+		generator.addOneAssertion(r.isLessThan(b, bBitwidth));
 		generator.addEqualityAssertion(q.mul(b).add(r), a);
 	}
 
--- JsnarkCircuitBuilder/src/examples/tests/Mod_Test.java
+++ JsnarkCircuitBuilder/src/examples/tests/Mod_Test.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import circuit.eval.CircuitEvaluator;
 import circuit.structure.CircuitGenerator;
 import circuit.structure.Wire;
+import examples.gadgets.ModConstantGadget;
 import examples.gadgets.ModGadget;
 
 
@@ -21,8 +22,8 @@ public class Mod_Test extends TestCase {
 	@Test
 	public void testCase1() {
 
-		int a = 62178522;
-		int b = 257;
+		int a = 1262178522;
+		int b = 257; // b will be an input to the circuit
 
 		CircuitGenerator generator = new CircuitGenerator("Mod_Test1") {
 
@@ -32,8 +33,10 @@ public class Mod_Test extends TestCase {
 			protected void buildCircuit() {
 
 				inputWires = createInputWireArray(2);
-				Wire r = new ModGadget(inputWires[0], (int) Math.ceil(Math.log10(a) / Math.log10(2)), inputWires[1],
-						(int) Math.ceil(Math.log10(b) / Math.log10(2))).getOutputWires()[0];
+//				Wire r = new ModGadget(inputWires[0], (int) Math.ceil(Math.log10(a) / Math.log10(2)), inputWires[1],
+//						(int) Math.ceil(Math.log10(b) / Math.log10(2))).getOutputWires()[0];
+				
+				Wire r = new ModGadget(inputWires[0],  inputWires[1], 32).getOutputWires()[0];
 				makeOutput(r);
 			}
 
@@ -52,5 +55,38 @@ public class Mod_Test extends TestCase {
 		Wire rWire = generator.getOutWires().get(0);
 		assertEquals(evaluator.getWireValue(rWire), BigInteger.valueOf(a % b));
 	}
+	
+	@Test
+	public void testCase2() {
+
+		int a = 1262178522;
+		int b = 257; // b will be a constant
+
+		CircuitGenerator generator = new CircuitGenerator("Mod_Test2") {
+
+			Wire[] inputWires;
+
+			@Override
+			protected void buildCircuit() {
+
+				inputWires = createInputWireArray(1);
+				Wire r = new ModConstantGadget(inputWires[0], 32, BigInteger.valueOf(b)).getOutputWires()[0];
+				makeOutput(r);
+			}
+
+			@Override
+			public void generateSampleInput(CircuitEvaluator e) {
+				e.setWireValue(inputWires[0], a);
+			}
+		};
+
+		generator.generateCircuit();
+		CircuitEvaluator evaluator = new CircuitEvaluator(generator);
+		generator.generateSampleInput(evaluator);
+		evaluator.evaluate();
+		Wire rWire = generator.getOutWires().get(0);
+		assertEquals(evaluator.getWireValue(rWire), BigInteger.valueOf(a % b));
+	}
+	
 
 }
